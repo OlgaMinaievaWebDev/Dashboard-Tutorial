56ac08d5e0e8afefcabdb0e426b40db18f50a07e
Olga <minaeva9@gmail.com>
2024-02-09 13:00:57 -0500

    new fetch req for crypto

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 fetch(
-  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=natfdfdure"
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data.urls.regular)
     document.body.style.backgroundImage = `url('${data.urls.regular}')`;
     document.getElementById("author").textContent = `By: ${data.user.name}`;
   })
   .catch((err) => {
     // This is where I can handle the error
     // Choose to use a default background image
-   document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1470770903676-69b98201ea1c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDc0OTk3NTZ8&ixlib=rb-4.0.3&q=80&w=1080')`;
+    document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1503614472-8c93d56e92ce?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDc1MDA2MzN8&ixlib=rb-4.0.3&q=80&w=1080')`;
+    document.getElementById("author").textContent = `By: John Lee`;
   });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error();
+    }
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((err) => {
+    document.getElementById("crypto").textContent =
+      "Not available at this moment";
+    console.error(err);
+  }); 
